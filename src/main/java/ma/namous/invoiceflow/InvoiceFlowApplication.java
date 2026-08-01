b6bc9a7601733364f0947a4bd062b8b13747cc56
NamousNassim <nassimnamousdev@gmail.com>
2026-08-01 10:43:13 -0700

    add readme with project goals

---
+++ src/main/java/ma/namous/invoiceflow/InvoiceFlowApplication.java
@@ -0,0 +1,10 @@
+package ma.namous.invoiceflow;
+
+
+public class InvoiceFlowApplication {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Hello from the InvoiceFlow Application");
+        }
+}
